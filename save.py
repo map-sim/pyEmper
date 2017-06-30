505fb72514f7b107dd95e944fc10e8910b2c3bfa
Krzysztof Czarnecki <krzycz@eti.pg.gda.pl>
2017-06-30 20:20:14 +0200

    + general info in save

--- save.py
+++ save.py
@@ -2,38 +2,42 @@
 
 import sqlite3 as lite
 import core, sys
+import datetime
 
-class EmpSave(arg, fname="save.db", author="anonymous"):
+class EmpSave():
 
-    def __init__(self):
+    def __init__(self, arg, fname="output.db", author="anonymous"):
         if type(arg) != type(core.EmpCore()):
             sys.stderr.write("arg has wrong type!")
             sys.exit(-1)
-        else:
-            self.core = arg
+        else: self.core = arg
 
-        con = None
+        self.con = None
         try:
-            con = lite.connect('test.db')
-    
-            self.cur = con.cursor()    
-            cur.execute('SELECT SQLITE_VERSION()')
- 
-            data = self.cur.fetchone()    
-            print("SQLite version: %s" % data)                
-            
-            self.__save_general_info()
+            self.con = lite.connect(fname)
+            self.cur = self.con.cursor()
+                        
+            self.__save_general_info(str(author))
 
-        except lite.Error, e:
+        except lite.Error:
             sys.stderr.write("error: %s" % e.args[0])
             sys.exit(-1)
     
         finally:    
-            if con: con.close()
+            if self.con:
+                self.con.close()
 
 
-    def __save_general_info(self):
-        "CREATE TABLE"
-    
+    def __save_general_info(self, author):
+        self.cur.execute("CREATE TABLE general_info(key TEXT, value TEXT)")
+        self.cur.execute("INSERT INTO general_info VALUES('author', '%s')" % author)
 
-# save(core.EmpCore())
+        now = datetime.datetime.now()
+        date_format = "%Y-%m-%d %H:%M"
+        date = now.strftime(date_format)
+        
+        self.cur.execute("INSERT INTO general_info VALUES('date format', '%s')" % date_format)
+        self.cur.execute("INSERT INTO general_info VALUES('date', '%s')" % date)
+        self.con.commit()
+        
+s = EmpSave(core.EmpCore())
