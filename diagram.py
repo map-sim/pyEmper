e0cc10a51fa9c4478c6242d562125512b76fd653
Krzysztof Czarnecki <krzycz@eti.pg.gda.pl>
2017-07-17 01:59:18 +0200

    + map

--- diagram.py
+++ diagram.py
@@ -3,6 +3,7 @@
 from tools import  call_error
 
 class EmpAtom:
+    def __repr__(self): return "x%d:y%d" % (self.x, self.y)
     def __init__(self, diagram, x, y, province, terrain):
         self.province = province
         self.terrain = terrain
@@ -10,53 +11,28 @@ class EmpAtom:
         
         self.n = x + y*self.diagram.width
         self.x,self.y = x,y
-        
+
+    def get_xy(self):
+        return (self.x, self.y)
+
     def check_border(self):        
-        try:
-            if not self.province is self.diagram.get_atom(self.x+1, self.y).province: return True
-        except AttributeError: return True     
-        except AssertionError: pass
-        try:
-            if not self.province is self.diagram.get_atom(self.x-1, self.y).province: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        try:
-            if not self.province is self.diagram.get_atom(self.x, self.y+1).province: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        try:
-            if not self.province is self.diagram.get_atom(self.x, self.y-1).province: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        return False
+        for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
+            try:
+                if not self.province is self.diagram.get_atom(self.x+x, self.y+y).province:
+                    return True
+            except AttributeError: return True     
+            except AssertionError: pass
 
     def check_coast(self):
         if self.terrain.ship<=0.5: return False
-        try:
-            a = self.diagram.get_atom(self.x+1, self.y)
-            if a.terrain.ship<=0.5: return True
-        except AttributeError: return True
-        except AssertionError: pass
+        for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
 
-        try:
-            a = self.diagram.get_atom(self.x-1, self.y)
-            if a.terrain.ship<=0.5: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        try:
-            a = self.diagram.get_atom(self.x, self.y+1)
-            if a.terrain.ship<=0.5: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        try:
-            a = self.diagram.get_atom(self.x, self.y-1)
-            if a.terrain.ship<=0.5: return True
-        except AttributeError: return True
-        except AssertionError: pass
-        return False
+            try:
+                a = self.diagram.get_atom(self.x+x, self.y+y)
+                if a.terrain.ship<=0.5: return True
+            except AttributeError: return True
+            except AssertionError: pass
 
-    def __repr__(self):
-        return "x%d:y%d" % (self.x, self.y)
     
 
 class EmpDiagram:
@@ -107,72 +83,72 @@ class EmpDiagram:
         self.set_area(pixels, p, t)
 
     def dilation(self, pixel, p, t):
-        atom = self.get_atom(*pixel)
-        if atom == None: return
-        if not atom.terrain is t: return
-        if not atom.province is p: return
-        checked = [False] * self.width * self.height
-        to_check,to_change = [pixel],[]
-        
-        while to_check:
-            xy = to_check.pop()
-            checked[xy[0] + xy[1]*self.width] = True
-            for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
-                nxy = (xy[0]+x,xy[1]+y)
-                try:
-                    if checked[nxy[0] + nxy[1]*self.width]: continue
-                except IndexError: continue
-                try: natom = self.get_atom(*nxy)
-                except AssertionError: continue
-                if natom==None: to_change.append(nxy)
-                elif not(atom.province is natom.province): to_change.append(nxy)
-                elif not(atom.terrain is natom.terrain): to_change.append(nxy)
-                else: to_check.append(nxy)        
-        self.set_area(to_change, p, t)
+        ribon = self.get_ribon(pixel)
+        self.set_area(ribon, p, t)
         print("dilatation")
 
     def filling(self, pixel, p, t):
+        group = self.get_area(pixel)
+        self.set_area(group, p, t)
+        print("filling")
+       
+        
+    def get_area(self, pixel):
         atom = self.get_atom(*pixel)
-        if atom == None: return
-        if p == None: return
-        if t == None: return
-        old_p = atom.province
-        old_t = atom.terrain
+        if atom == None:
+            print("None cannot be got")
+            return []
 
-        to_check = [atom]
         checked = []
- 
+        to_check = [atom]
+        found = [atom.get_xy()]
+        
         while to_check:
             a = to_check.pop()
             checked.append(a)
-            a.province,a.terrain = p,t
-
             for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
-
                 try:
                     an = self.get_atom(a.x+x,a.y+y)
                     if an in checked: continue
-                    elif an.province is old_p and an.terrain is old_t:
+                    elif an.province is atom.province and an.terrain is atom.terrain:
                         to_check.append(an)
+                        found.append(an.get_xy())
                 except AttributeError: continue
                 except AssertionError: continue
-        print("filling")
-       
+        return found 
+
+    def get_ribon(self, pixel):
+        atom = self.get_atom(*pixel)
+        if atom == None:
+            print("None cannot be got")
+            return []
+
+        checked = []
+        to_check = [atom]
+        found = [atom.get_xy()]
+
+        while to_check:
+            a = to_check.pop()
+            checked.append(a)
+            for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
+                try:
+                    an = self.get_atom(a.x+x,a.y+y)
+                    if an in checked: continue
+                    elif an.province is atom.province and an.terrain is atom.terrain:
+                        to_check.append(an)
+                    else: found.append(an.get_xy())
+                except AttributeError: continue
+                except AssertionError: continue
+        return found 
+        
     def smooth_by_province(self):
         for a in self.atoms:
-            if a==None:  continue                
-            try: provs = [self.get_atom(a.x+1, a.y).province]
-            except AttributeError: provs = []
-            except AssertionError: provs = []
-            try: provs.append(self.get_atom(a.x-1, a.y).province)
-            except AttributeError: pass
-            except AssertionError: pass
-            try: provs.append(self.get_atom(a.x, a.y+1).province)
-            except AttributeError: pass
-            except AssertionError: pass
-            try: provs.append(self.get_atom(a.x, a.y-1).province)
-            except AttributeError: pass
-            except AssertionError: pass
+            if a==None:  continue
+            provs = []
+            for x,y in [(0,1), (0,-1), (1,0), (-1,0)]:
+                try: provs.append(self.get_atom(a.x+x, a.y+y).province)
+                except AttributeError: continue
+                except AssertionError: continue
             
             dp = {}
             for p in provs:
